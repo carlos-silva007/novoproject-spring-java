36ebfe161b17e06527240eca584f617cff3f4482
carlos-silva007 <carloos19@gmail.com>
2022-03-08 11:44:22 -0300

    create new project com spring e java

---
+++ src/main/java/com/example/curso/ProjetoSpringJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringJavaApplication.class, args);
+	}
+
+}
