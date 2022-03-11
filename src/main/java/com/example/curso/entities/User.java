44b435a180ded275bb8910178e39b0a1ec0da844
carlos-silva007 <carloos19@gmail.com>
2022-03-11 16:12:56 -0300

    create a new classa order

--- src/main/java/com/example/curso/entities/User.java
+++ src/main/java/com/example/curso/entities/User.java
@@ -1,13 +1,18 @@
 package com.example.curso.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -20,6 +25,10 @@ public class User implements Serializable {
 	private String email;
 	private String telefone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+		
 
 	public User() {
 	}
@@ -102,5 +111,9 @@ public class User implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 }
